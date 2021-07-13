2066002469bd6d8d241afbbd32b0113453487062
Oliver Klukas <oliverklukas@t-online.de>
2021-07-13 12:28:13 +0200

    full realtime database feature support enabled

--- lib/controller/firestore_controller.dart
+++ lib/controller/firestore_controller.dart
@@ -2,19 +2,24 @@ import 'package:book_finder/models/book.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreController {
-  /// Reference to firebase library of books
+  /// Reference to firesore library of books
   final libraryRef = FirebaseFirestore.instance.collection('library').withConverter<Book>(
     fromFirestore: (snapshot, _) => Book.fromJson(snapshot.data()!),
-    toFirestore: (movie, _) => movie.toJson(),
+    toFirestore: (book, _) => book.toJson(),
   );
 
-  /// Method to add a book to the firebase library
+  /// Method to add a book to the firesore library
   Future<void> addBook(Book book){
     return libraryRef.add(book);
   }
 
-  /// Method to delete a book to the firebase library
+  /// Method to delete a book to the firesore library
   Future<void> deleteBook(String bookId){
     return libraryRef.doc(bookId).delete();
+  }
+
+  /// Method to update a book in the firesore library
+  Future<void> updateBook(Book book){
+    return libraryRef.doc(book.id).update(book.toJson());
   }
 }
